01786121d74f1ee85d02733ce9f98a1b38cf5bb3
Fidel <fidellauheinz@gmail.com>
2024-12-13 13:55:26 +0100

    corección CDN taliwind

--- tuEquipo.php
+++ tuEquipo.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- TAILWIND -->
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="./js/validacionEquipo.js"></script>
     <title>Crear equipo</title>
