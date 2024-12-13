0c5add10f5039b0462024e55604ac7747c9806ce
Carla <carluchi012002@gmail.com>
2024-12-13 10:51:53 +0100

    Corrijo las variables de addUsers.php

--- php/addUsers.php
+++ php/addUsers.php
@@ -7,16 +7,16 @@ include_once "config.php";
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 
-    $name = $_POST("name");
-    $email = $_POST("email");
-    $pass = $_POST("pass");
-    $birthDate = $_POST("birthDate");
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
+    $birthDate = $_POST["birthDate"];
     $hash = password_hash($pass, PASSWORD_DEFAULT);
- 
+
     $sql = "INSERT INTO players (name, email, pass, birth_date) VALUES (?,?,?,?)";
     $stmt = $conn->prepare($sql);
 
-    $stmt->bind_param("sssd", $name, $email, $hash, $birthDate);
+    $stmt->bind_param("ssss", $name, $email, $hash, $birthDate);
 
     if ($stmt->execute()) {
         echo json_encode(["success" => true]);
