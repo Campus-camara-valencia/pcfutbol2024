d333267161ff054fddb3bed73220273f657476c1
Maina <bozamaina@gmail.com>
2024-12-16 12:33:06 +0100

    Cambio de nombres de cada php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ include_once "./php/sessionlogin.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" type="text/css" href="css/styles_login.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 
 <body class="flex flex-col items-center justify-center min-h-screen p-4">
